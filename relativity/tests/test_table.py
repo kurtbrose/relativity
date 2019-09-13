ffa34d4fa44362156b87863ee75f572a4e149181
Kurt Rose <kurt.rose@simplelegal.com>
2019-09-13 15:09:32 -0700

    table stuff

--- relativity/tests/test_table.py
+++ relativity/tests/test_table.py
@@ -18,9 +18,36 @@ def test():
     assert set(users) == set([(2, 'carol', 42)])
 
 
-"""
+
 def test_new_api():
-    switchboard = Table(['vendor-id', 'matter-id'])
-    matter_oa_foo = Table(['matter-id', 'foo'])
-    switchboard = switchboard.join(matter_oa_foo, on="matter-id")
-"""
+    # 1 - fill in base rows
+    # structure for switchboard is constant per namespace
+    # and is filled by one DB hit up front
+    switchboard = namespace2switchboard_table(target, namespace)
+
+    # 2 - analyze ruleset to get all required fetchers
+    fetchers = derive_fetchers(ruleset)
+
+    # 3 - perform database ops via fetchers, grab data
+    fetched_tables = []
+    for fetcher in fetchers:
+        # things FROM switchboard
+        # maybe we can give the whole switchboard
+        # and they extract what they need (e.g. matter-id)
+        fetched_tables.append(fetcher.fetch(switchboard))
+
+    # 4 - build big combo table
+    full_table = switchboard
+    for fetched_table in fetched_tables:
+        # maybe we can avoid explicit "on" by natural join
+        # -- join on columns with the same name
+        full_table = full_table.join(
+            fetched_table, on=fetched_table.inputs)
+
+    # 5 - evaluate expressions row-by-row
+    for row in full_table:
+        if eval("foo > 10 and bar == 3", row):
+            result.add(row)
+        # extra credit -- use set intersection for perf
+
+
