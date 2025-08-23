2775ec730bbee7c55cbf3418df06f2d08e867437
Kurt Rose <kurt@kurtrose.com>
2025-08-22 20:35:14 -0700

    Refactor schema tables

--- relativity/schema.py
+++ relativity/schema.py
@@ -1,28 +1,43 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Dict, Iterator, Type, Any
+from typing import ClassVar, Iterator
 from typing import dataclass_transform
+
+
+@dataclass_transform()
+class Table:
+    """Base class for schema-bound tables."""
+
+    __schema__: ClassVar["Schema"]
+
+    def __init_subclass__(cls, *, schema: "Schema", **kw) -> None:  # type: ignore[override]
+        super().__init_subclass__(**kw)
+        dataclass(eq=False, frozen=True)(cls)
+        cls.__schema__ = schema
+        schema._tables.setdefault(cls, {})
 
 
 class Schema:
     def __init__(self) -> None:
-        self._tables: Dict[type, Dict[int, Any]] = {}
+        self._tables: dict[type[object], dict[int, object]] = {}
+
+    @property
+    def Table(self) -> type[Table]:
         schema = self
 
-        @dataclass_transform()
-        class Table:
-            def __init_subclass__(cls, **kwargs):  # type: ignore[override]
-                dataclass(eq=False, frozen=True)(cls)
-                schema._tables.setdefault(cls, {})
+        class _Bound(Table, schema=schema):  # type: ignore[misc]
+            def __init_subclass__(cls, **kw) -> None:
+                super().__init_subclass__(schema=schema, **kw)
 
-        self.Table = Table
+        return _Bound
 
-    def add(self, row: Any) -> None:
-        self._tables[row.__class__][id(row)] = row
+    def add(self, row: object) -> None:
+        self._tables[type(row)][id(row)] = row
 
-    def remove(self, row: Any) -> None:
-        del self._tables[row.__class__][id(row)]
+    def remove(self, row: object) -> None:
+        del self._tables[type(row)][id(row)]
 
-    def all(self, table: Type[Any]) -> Iterator[Any]:
+    def all(self, table: type[object]) -> Iterator[object]:
         return iter(self._tables.get(table, {}).values())
+
